c8b3ef27992a7af050a069af51e469cea9b1d15a
Cristofer Binimelis <cristoferbinimelis@gmail.com>
2024-07-30 23:21:05 -0400

    Modulo 2 - Sesion 9

---
+++ Módulo 2/M2 - Sesion 9/M2 Sesion 9.md
@@ -0,0 +1,40 @@
+# Módulo II - Sesión VIII
+
+## Agenda
+
+### Sincrónica
+
+- Funciones
+- Arreglos
+- Bucles
+
+### Asincrónica
+
+- A
+
+## Funciones
+
+Las funciones son fragmentos de código que se repetirá varias veces dentro de la ejecución del codigo. La principal tarea de una funcion, es la de ahorrar el trabajo de tener que repetir dicho código cada vez quye se requiera utilizarlo.
+La estructura básica de una función es la siguiente:
+
+```javascript
+function nombreFuncion(variable) {
+  // Codigo a ejecutar
+}
+```
+
+## Arreglos
+
+Son la estructura de datos más básica que existe en JavaScript. Admiten datos de cualquier tipo, pudiendo almacenar la información en distintas dimensiones
+
+## Bucles
+
+Son bloques de codigos que se ejecutarán repetidamente de manera automática siempre y cuando se haya cumplido con la condición necesaria para su ejecución
+
+La estructura básica de un bucle `for` es la siguiente:
+
+```javascript
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+```
---
+++ Módulo 2/M2 - Sesion 9/sesion_9/assets/js/script.js
@@ -0,0 +1,65 @@
+function saludo() {
+  console.log("Hola Mundo (otra veh)");
+}
+
+saludo();
+saludoPersona("Miguel", "Soto");
+
+function saludoPersona(nombre, apellido) {
+  console.log(
+    `Hola ${nombre} ${apellido}, el plato para hoy es: ${menuDiario()}`
+  );
+}
+
+function menuDiario() {
+  return "Papas mayo";
+}
+
+let alumnos = Array();
+console.log(alumnos);
+let profesores = [];
+console.log(profesores);
+
+let alumnos2 = Array("Patricio", "Hector");
+let profesores2 = ["Gabriel"];
+console.log(alumnos2, profesores2);
+
+let edades = [4, 1, 19, 12, 85, 33, 18, 15, 20, 29];
+let menoresDeEdad = edades.filter((elemento) => elemento < 18);
+console.log(menoresDeEdad);
+
+let valorRecorrido = edades.length;
+for (let index = 0; index < edades.length; index++) {
+  console.log(edades[index]);
+}
+
+let contador = 0;
+while (contador < 5) {
+  console.log(`Bucle #${contador + 1}`);
+  contador++;
+}
+
+let isExit = false;
+let cnt = 0;
+while (isExit === false) {
+  console.log(`Bucle v2 #${cnt + 1}`);
+  cnt++;
+  if (cnt === 10) {
+    isExit = true;
+  }
+}
+
+let num = 0;
+
+do {
+  num = lanzarDado();
+  console.log();
+} while (num % 2 === 0);
+
+function lanzarDado() {
+  return 3;
+}
+
+var creandoElemento = document.createElement("p");
+creandoElemento.textContent = "Este nodo fue creado desde JavaScript";
+document.body.appendChild(creandoElemento);
