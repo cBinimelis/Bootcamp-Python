921adb0c3782d4d2f4830a65c6acb4aed59cc503
Cristofer Binimelis <cristoferbinimelis@gmail.com>
2024-08-19 20:40:20 -0400

    Consolidación finalizada

--- Módulo 2/M2 Consolidacion/assets/js/script.js
+++ Módulo 2/M2 Consolidacion/assets/js/script.js
@@ -1,10 +1,12 @@
 $(document).ready(function () {
   $("#Logo").delay(800).fadeIn();
 
+  /*
   $("html, body").animate(
     {
       scrollTop: $("#intro").offset().top,
     },
     2000
   );
+  */
 });
