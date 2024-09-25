b03dd6f7ed6d5b49d9358b378b7310a05d15b6e7
Cristofer Binimelis <cristoferbinimelis@gmail.com>
2024-09-24 22:00:15 -0300

    M4 S3 Teoría

--- Modulo 4/M4 - Sesion 02/m4s2drilling.py
+++ Modulo 4/M4 - Sesion 02/m4s2drilling.py
@@ -13,6 +13,43 @@ class Person:
     def set_nombre(self, nombre):
         self.nombre = nombre
 
+    def get_apellidos(self):
+        return self.apellidos
+
+    def set_apellidos(self, apellidos):
+        self.apellidos = apellidos
+
+    def get_sexo(self):
+        return self.sexo
+
+    def set_sexo(self, sexo):
+        self.sexo = sexo
+
+    def get_edad(self):
+        return self.edad
+
+    def set_edad(self, edad):
+        self.edad = edad
+
+    def get_estatura(self):
+        return self.estatura
+
+    def set_estatura(self, estatura):
+        self.estatura = estatura
+
+    def get_peso(self):
+        return self.peso
+
+    def set_peso(self, peso):
+        self.peso = peso
+
 
 persona_1 = Person("Pedro", "Vivas", "Masculino", 20, 1.78, 68)
 persona_2 = Person("Juan", "Camargo", "Masculino", 18, 1.8, 75)
+
+print("Valores originales:", persona_1.get_edad(), persona_2.get_apellidos())
+
+persona_1.set_edad("21")
+persona_2.set_apellidos("Santiago")
+
+print("Valores modificados:", persona_1.get_edad(), persona_2.get_apellidos())
