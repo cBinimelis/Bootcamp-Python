b78e6ee1a892160a3d448238721b01f8e7340b7b
Cristofer Binimelis <cristoferbinimelis@gmail.com>
2024-12-14 22:23:59 -0300

    Album CRUD complete

--- Modulo_7/M7_Chinook/music/urls.py
+++ Modulo_7/M7_Chinook/music/urls.py
@@ -5,9 +5,18 @@ app_name = "music"
 
 urlpatterns = [
     path("", views.home, name="home"),
+    
+    #ARTIST URLS
     path("artist/", views.artist_list, name="artist_list"),
     path("artist/create/", views.artist_create, name="artist_create"),
     path("artist/<int:pk>/edit", views.artist_edit, name="artist_edit"),
     path("artist/<int:pk>/delete", views.artist_delete, name="artist_delete"),
     path("artist/<int:pk>", views.artist_view, name="artist_view"),
+    
+    #ALBUM URLS
+    path("album/", views.album_list, name="album_list"),
+    path("album/create/", views.album_create, name="album_create"),
+    path("album/<int:pk>/edit", views.album_edit, name="album_edit"),
+    path("album/<int:pk>/delete", views.album_delete, name="album_delete"),
+    path("album/<int:pk>", views.album_view, name="album_view"),
 ]
--- Modulo_7/M7_Chinook/music/views.py
+++ Modulo_7/M7_Chinook/music/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Artist
-from .forms import ArtistForm
+from .models import Artist, Album
+from .forms import ArtistForm, AlbumForm
 
 
 # Create your views here.
 def home(request):
     return render(request, "music/home.html")
 
-
+#ARTIST ----------------------------------------------------------------
 def artist_list(request):
     artists = Artist.objects.all()
     context = {"artists": artists}
@@ -52,3 +52,48 @@ def artist_view(request, pk):
     artist = Artist.objects.get(id=pk)
     context = {"artist": artist}
     return render(request, "music/artist/artist_view.html", context)
+
+
+#ALBUM ----------------------------------------------------------------
+def album_list(request):
+    albums = Album.objects.all()
+    context={'albums':albums}
+    return render(request,'music/album/album_list.html',context)
+
+def album_create(request):
+    if request.method == 'POST':
+        form = AlbumForm(request.POST)
+        form.save()
+        return redirect('album_list')
+    else:
+        form = AlbumForm()
+    context = {'form':form}
+    return redirect(request,'music/album/album_create,html',context)
+
+
+def album_edit(request,pk):
+    album = Album.objects.get(id=pk)
+    if request.method == 'POST':
+        form = AlbumForm(request.POST,instance=album)
+        form.save()
+        return redirect('album_list')
+    else:
+        form = AlbumForm(instance=album)
+    context = {'form':form}
+    return redirect(request,'music/album/album_create,html',context)
+
+
+def album_delete(request, pk):
+    album = Album.objects.get(id=pk)
+    if request.method == "POST":
+        album.delete()
+        return redirect("album_list")
+    else:
+        context = {"album": album}
+        return render(request, "music/album/album_confirm_delete.html", context)
+
+
+def album_view(request, pk):
+    album = Album.objects.get(id=pk)
+    context = {"album": album}
+    return render(request, "music/album/album_view.html", context)
